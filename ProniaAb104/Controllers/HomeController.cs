e90fb21e0ced3c22c55d67dc8302327a5fcaffdb
tofiq7770 <tofigtn@code.edu.az>
2023-11-22 21:38:43 +0400

    Added Slider

--- ProniaAb104/Controllers/HomeController.cs
+++ ProniaAb104/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProniaAb104.Models;
 
 namespace ProniaAb104.Controllers
 {
@@ -6,7 +7,50 @@ namespace ProniaAb104.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<Slide> slides = new List<Slide>
+            {
+                new Slide
+                {
+                    Id = 1,
+                    Title ="Title 1",
+                    SubTitle="SubTitle 1",
+                    Description ="Very Good 1",
+                    Image="slide 1.jfif",
+                    Order = 3,
+                },
+
+                new Slide
+                {
+                    Id = 2,
+                    Title ="Title 2",
+                    SubTitle="SubTitle 2",
+                    Description ="Very Good 2",
+                    Image="slide 2.jfif",
+                    Order = 1,
+                },
+
+                new Slide
+                {
+                    Id = 3,
+                    Title ="Title 3",
+                    SubTitle="SubTitle 3",
+                    Description ="Very Good 3",
+                    Image="slide 3.jfif",
+                    Order = 4,
+                },
+                
+                new Slide
+                {
+                    Id = 4,
+                    Title ="Title 4",
+                    SubTitle="SubTitle 4",
+                    Description ="Very Good 4",
+                    Image="slide 4.jfif",
+                    Order = 2,
+                }
+            };
+            return View(slides);
+
         }
         public IActionResult About()
         {
