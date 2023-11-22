c0651209a0f0261982ef5baf8ab566d6c0a0da00
tofiq7770 <tofigtn@code.edu.az>
2023-11-23 01:53:46 +0400

    Created AppDbContext

--- ProniaAb104/Controllers/HomeController.cs
+++ ProniaAb104/Controllers/HomeController.cs
@@ -1,55 +1,71 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProniaAb104.DAL;
 using ProniaAb104.Models;
 
 namespace ProniaAb104.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            List<Slide> slides = new List<Slide>
-            {
-                new Slide
-                {
-                    Id = 1,
-                    Title ="Title 1",
-                    SubTitle="SubTitle 1",
-                    Description ="Very Good 1",
-                    Image="slide 1.jfif",
-                    Order = 3,
-                },
+            #region SQL
+            //List<Slide> slides = new List<Slide>
+            //{
+            //    new Slide
+            //    {
 
-                new Slide
-                {
-                    Id = 2,
-                    Title ="Title 2",
-                    SubTitle="SubTitle 2",
-                    Description ="Very Good 2",
-                    Image="slide 2.jfif",
-                    Order = 1,
-                },
+            //        Title ="Title 1",
+            //        SubTitle="SubTitle 1",
+            //        Description ="Very Good 1",
+            //        Image="slide 1.jfif",
+            //        Order = 3,
+            //    },
 
-                new Slide
-                {
-                    Id = 3,
-                    Title ="Title 3",
-                    SubTitle="SubTitle 3",
-                    Description ="Very Good 3",
-                    Image="slide 3.jfif",
-                    Order = 4,
-                },
-                
-                new Slide
-                {
-                    Id = 4,
-                    Title ="Title 4",
-                    SubTitle="SubTitle 4",
-                    Description ="Very Good 4",
-                    Image="slide 4.jfif",
-                    Order = 2,
-                }
-            };
-            return View(slides.Take(4).OrderBy(s=>s.Order).ToList());
+            //    new Slide
+            //    {
+
+            //        Title ="Title 2",
+            //        SubTitle="SubTitle 2",
+            //        Description ="Very Good 2",
+            //        Image="slide 2.jfif",
+            //        Order = 1,
+            //    },
+
+            //    new Slide
+            //    {
+
+            //        Title ="Title 3",
+            //        SubTitle="SubTitle 3",
+            //        Description ="Very Good 3",
+            //        Image="slide 3.jfif",
+            //        Order = 4,
+            //    },
+
+            //    new Slide
+            //    {
+
+            //        Title ="Title 4",
+            //        SubTitle="SubTitle 4",
+            //        Description ="Very Good 4",
+            //        Image="slide 4.jfif",
+            //        Order = 2,
+            //    }
+            //};
+
+            //_context.Slides.AddRange(slides);
+            //_context.SaveChanges();
+            #endregion
+
+            List<Slide> slides = _context.Slides.OrderBy(s=>s.Order).Take(3).ToList();
+
+            //return View(slides.Take(4).OrderBy(s=>s.Order).ToList()); sqlden datani goturub orderby edirik
+            return View(slides);
 
         }
         public IActionResult About()
--- ProniaAb104/Program.cs
+++ ProniaAb104/Program.cs
@@ -1,9 +1,15 @@
 
 
 
+using Microsoft.EntityFrameworkCore;
+using ProniaAb104.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
