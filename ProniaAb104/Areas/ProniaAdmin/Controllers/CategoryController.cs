7efd6708114cdd32279b11fcbd09d626a588ef97
tofiq7770 <tofigtn@code.edu.az>
2023-12-03 14:57:32 +0400

    Update Crud

---
+++ ProniaAb104/Areas/ProniaAdmin/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProniaAb104.DAL;
+using ProniaAb104.Models;
+
+namespace ProniaAb104.Areas.ProniaAdmin.Controllers
+{
+    [Area("ProniaAdmin")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await _context.Categories.Include(c=>c.Products).ToListAsync();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return Json(category);
+        }
+    }
+}
--- ProniaAb104/Models/Category.cs
+++ ProniaAb104/Models/Category.cs
@@ -7,7 +7,7 @@ namespace ProniaAb104.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Ad mutleq daxil edilmeldir")]
-        [MaxLength(25, ErrorMessage = "Uznlugu 25 xarakterden cox olmamalidir")]
+        [MaxLength(25, ErrorMessage = "Uzunlugu 25 xarakterden cox olmamalidir")]
         public string Name { get; set; }
         public List<Product>? Products { get; set; }
     }
