3f2f5d3173d370fe624f5c73e615efbe5f0fb217
tofiq7770 <tofigtn@code.edu.az>
2023-12-03 16:48:28 +0400

    Added Crud

--- ProniaAb104/Areas/ProniaAdmin/Controllers/CategoryController.cs
+++ ProniaAb104/Areas/ProniaAdmin/Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace ProniaAb104.Areas.ProniaAdmin.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            List<Category> categories = await _context.Categories.Include(c=>c.Products).ToListAsync();
+            List<Category> categories = await _context.Categories.Include(c => c.Products).ToListAsync();
             return View(categories);
         }
         public IActionResult Create()
@@ -23,13 +23,80 @@ namespace ProniaAb104.Areas.ProniaAdmin.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Category category)
+        public async Task<IActionResult> Create(Category category)
         {
             if (!ModelState.IsValid)
             {
                 return View();
             }
-            return Json(category);
+
+            bool result = _context.Categories.Any(c => c.Name.ToLower().Trim() == category.Name.ToLower().Trim());
+            if (result)
+            {
+                ModelState.AddModelError("Name", "Category artiq movcuddur");
+                return View();
+            }
+
+            _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Update(int id, Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            Category existed = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (existed == null) return NotFound();
+
+            bool result = _context.Categories.Any(c => c.Name.ToLower().Trim() == category.Name.ToLower().Trim() && c.Id != id);
+
+            if (result)
+            {
+                ModelState.AddModelError("Name", "Bu Categoryde ad movcuddur");
+                return View();
+            }
+            existed.Name = category.Name;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+            //return View("Index");
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            Category existed = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (existed is null) return NotFound();
+
+            _context.Categories.Remove(existed);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+        public async Task<IActionResult> Detail(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null) return NotFound();
+
+            return View(category);
         }
     }
 }
