2c60a7e09c09d4f33952c4f9cee01c28c517ff21
tofiq7770 <tofigtn@code.edu.az>
2023-12-06 05:06:59 +0400

    created products

--- ProniaAb104/Areas/ProniaAdmin/Controllers/ProductController.cs
+++ ProniaAb104/Areas/ProniaAdmin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ProniaAb104.Areas.ProniaAdmin.ViewModels;
 using ProniaAb104.DAL;
 using ProniaAb104.Models;
 
@@ -20,20 +21,44 @@ namespace ProniaAb104.Areas.ProniaAdmin.Controllers
         {
             List<Product> products = await _context.Products
                 .Include(p => p.Category)
-                .Include(p => p.ProductImages.Where(pi=>pi.IsPrimary==true))
+                .Include(p => p.ProductImages.Where(pi => pi.IsPrimary == true))
                 .ToListAsync();
-           
+
             return View(products);
         }
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
+            ViewBag.Categories = await _context.Categories.ToListAsync();
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Product product)
+        public async Task<IActionResult> Create(CreateProductVM productVM)
         {
-            if (!ModelState.IsValid) return BadRequest();
-            return View();
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = await _context.Categories.ToListAsync();
+                return View();
+            }
+            bool result = await _context.Categories.AnyAsync(c => c.Id == productVM.CategoryId);
+            if (!result)
+            {
+                ViewBag.Categories = await _context.Categories.ToListAsync();
+                ModelState.AddModelError("CategoryId", "Bu Id-li category artiq movcuddur}");
+                return View();
+            }
+            Product product = new()
+            {
+                Name = productVM.Name,
+                Price = productVM.Price,
+                SKU = productVM.SKU,
+                CategoryId = (int)productVM.CategoryId,
+                Description = productVM.Description,
+            };
+            
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ ProniaAb104/Areas/ProniaAdmin/ViewModels/Product/CreateProductVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProniaAb104.Areas.ProniaAdmin.ViewModels
+{
+    public class CreateProductVM
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string SKU { get; set; }
+        [Required]
+        public int? CategoryId { get; set; }
+    }
+}
